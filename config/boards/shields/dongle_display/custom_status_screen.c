7170f870edbc5a916215ae45773c9498f895c9cd
William Bonfim <gbo.william@gmail.com>
2024-10-28 19:45:24 +0000

    wpm alignment

--- config/boards/shields/dongle_display/custom_status_screen.c
+++ config/boards/shields/dongle_display/custom_status_screen.c
@@ -66,8 +66,8 @@ lv_obj_t *zmk_display_status_screen() {
 
     zmk_widget_wpm_status_init(&wpm_status_widget, screen);
     //lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), LV_ALIGN_BOTTOM_LEFT, 15, -18);
-    lv_obj_align_to(zmk_widget_wpm_status_obj(&wpm_status_widget), zmk_widget_bongo_cat_obj(&bongo_cat_widget), LV_ALIGN_BOTTOM_LEFT, 0, 5);
-    //lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), LV_ALIGN_BOTTOM_RIGHT, 0, 0);
+    //lv_obj_align_to(zmk_widget_wpm_status_obj(&wpm_status_widget), zmk_widget_bongo_cat_obj(&bongo_cat_widget), LV_ALIGN_BOTTOM_LEFT, 0, 5);
+    lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), LV_ALIGN_BOTTOM_RIGHT, -15, 0);
     
     return screen;
 }
