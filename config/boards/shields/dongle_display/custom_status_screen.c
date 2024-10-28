b485b7294be0d1de2db3ee78f32828709d3c7944
williambonfim <gbo.william@gmail.com>
2024-10-28 14:56:13 +0000

    update

--- config/boards/shields/dongle_display/custom_status_screen.c
+++ config/boards/shields/dongle_display/custom_status_screen.c
@@ -63,7 +63,7 @@ lv_obj_t *zmk_display_status_screen() {
 
     zmk_widget_wpm_status_init(&wpm_status_widget, screen);
     //lv_obj_align(zmk_widget_wpm_status_obj(&wpm_status_widget), NULL, LV_ALIGN_IN_BOTTOM_RIGHT, 0, 0);
-    lv_obj_align_to(zmk_widget_layer_status_obj(&wpm_status_widget), zmk_widget_bongo_cat_obj(&bongo_cat_widget), LV_ALIGN_BOTTOM_LEFT, 0, 5)
+    lv_obj_align_to(zmk_widget_layer_status_obj(&wpm_status_widget), zmk_widget_bongo_cat_obj(&bongo_cat_widget), LV_ALIGN_BOTTOM_LEFT, 0, 5);
 
     zmk_widget_dongle_battery_status_init(&dongle_battery_status_widget, screen);
     lv_obj_align(zmk_widget_dongle_battery_status_obj(&dongle_battery_status_widget), LV_ALIGN_TOP_RIGHT, 0, 0);
