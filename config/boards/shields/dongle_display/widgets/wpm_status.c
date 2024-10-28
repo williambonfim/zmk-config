28d3ece1f3ddf59ff59271d7be0da4f1c1480fe6
williambonfim <gbo.william@gmail.com>
2024-10-28 14:51:41 +0000

    wpm update

--- config/boards/shields/dongle_display/widgets/wpm_status.c
+++ config/boards/shields/dongle_display/widgets/wpm_status.c
@@ -4,82 +4,55 @@
  * SPDX-License-Identifier: MIT
  */
 
-#include <device.h>
-#include <init.h>
-#include <zephyr/kernel.h>
-
 #include <logging/log.h>
-
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
+#include <zmk/display.h>
+#include <zmk/display/widgets/wpm_status.h>
 #include <zmk/events/wpm_state_changed.h>
-
+#include <zmk/event_manager.h>
+#include <zmk/endpoints.h>
 #include <zmk/wpm.h>
 
-#define WPM_UPDATE_INTERVAL_SECONDS 1
-#define WPM_RESET_INTERVAL_SECONDS 5
+static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
-// See https://en.wikipedia.org/wiki/Words_per_minute
-// "Since the length or duration of words is clearly variable, for the purpose of measurement of
-// text entry, the definition of each "word" is often standardized to be five characters or
-// keystrokes long in English"
+struct wpm_status_state {
+    uint8_t wpm;
+};
 
-#define CHARS_PER_WORD 5.0
+struct wpm_status_state wpm_status_get_state(const zmk_event_t *eh) {
+    return (struct wpm_status_state){.wpm = zmk_wpm_get_state()};
+};
 
-static uint8_t wpm_state = -1;
-static uint8_t last_wpm_state;
-static uint8_t wpm_update_counter;
-static uint32_t key_pressed_count;
+void set_wpm_symbol(lv_obj_t *label, struct wpm_status_state state) {
+    char text[4] = {};
 
-int zmk_wpm_get_state() { return wpm_state; }
+    LOG_DBG("WPM changed to %i", state.wpm);
+    snprintf(text, sizeof(text), "%i", state.wpm);
 
-int wpm_event_listener(const zmk_event_t *eh) {
-    const struct zmk_keycode_state_changed *ev = as_zmk_keycode_state_changed(eh);
-    if (ev) {
-        // count only key up events
-        if (!ev->state) {
-            key_pressed_count++;
-            LOG_DBG("key_pressed_count %d keycode %d", key_pressed_count, ev->keycode);
-        }
-    }
+    lv_label_set_text(label, text);
+    lv_obj_align(label, NULL, LV_ALIGN_IN_BOTTOM_RIGHT, 0, 0);
+}
+
+void wpm_status_update_cb(struct wpm_status_state state) {
+    struct zmk_widget_wpm_status *widget;
+    SYS_SLIST_FOR_EACH_CONTAINER(&widgets, widget, node) { set_wpm_symbol(widget->obj, state); }
+}
+
+ZMK_DISPLAY_WIDGET_LISTENER(widget_wpm_status, struct wpm_status_state, wpm_status_update_cb,
+                            wpm_status_get_state)
+ZMK_SUBSCRIPTION(widget_wpm_status, zmk_wpm_state_changed);
+
+int zmk_widget_wpm_status_init(struct zmk_widget_wpm_status *widget, lv_obj_t *parent) {
+    widget->obj = lv_label_create(parent, NULL);
+    lv_label_set_align(widget->obj, LV_LABEL_ALIGN_RIGHT);
+
+    lv_obj_set_size(widget->obj, 40, 15);
+
+    sys_slist_append(&widgets, &widget->node);
+
+    widget_wpm_status_init();
     return 0;
 }
 
-void wpm_work_handler(struct k_work *work) {
-    wpm_update_counter++;
-    wpm_state = (key_pressed_count / CHARS_PER_WORD) /
-                (wpm_update_counter * WPM_UPDATE_INTERVAL_SECONDS / 60.0);
-
-    if (last_wpm_state != wpm_state) {
-        LOG_DBG("Raised WPM state changed %d wpm_update_counter %d", wpm_state, wpm_update_counter);
-
-        ZMK_EVENT_RAISE(
-            new_zmk_wpm_state_changed((struct zmk_wpm_state_changed){.state = wpm_state}));
-
-        last_wpm_state = wpm_state;
-    }
-
-    if (wpm_update_counter >= WPM_RESET_INTERVAL_SECONDS) {
-        wpm_update_counter = 0;
-        key_pressed_count = 0;
-    }
-}
-
-K_WORK_DEFINE(wpm_work, wpm_work_handler);
-
-void wpm_expiry_function() { k_work_submit(&wpm_work); }
-
-K_TIMER_DEFINE(wpm_timer, wpm_expiry_function, NULL);
-
-int wpm_init() {
-    wpm_state = 0;
-    wpm_update_counter = 0;
-    k_timer_start(&wpm_timer, K_SECONDS(WPM_UPDATE_INTERVAL_SECONDS),
-                  K_SECONDS(WPM_UPDATE_INTERVAL_SECONDS));
-    return 0;
-}
-
-ZMK_LISTENER(wpm, wpm_event_listener);
-ZMK_SUBSCRIPTION(wpm, zmk_keycode_state_changed);
-
-SYS_INIT(wpm_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
+lv_obj_t *zmk_widget_wpm_status_obj(struct zmk_widget_wpm_status *widget) { return widget->obj; }
