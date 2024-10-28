5eb83d2d8d02d025891cf0cb7000f64ecc156422
williambonfim <gbo.william@gmail.com>
2024-10-28 07:44:23 +0000

    layer widget update location

--- config/boards/shields/dongle_display/custom_status_screen.c
+++ config/boards/shields/dongle_display/custom_status_screen.c
@@ -57,7 +57,7 @@ lv_obj_t *zmk_display_status_screen() {
     //lv_obj_align_to(zmk_widget_layer_status_obj(&layer_status_widget), zmk_widget_bongo_cat_obj(&bongo_cat_widget), LV_ALIGN_BOTTOM_LEFT, 0, 5);
     
     //lv_obj_align(zmk_widget_layer_status_obj(&layer_status_widget), LV_ALIGN_BOTTOM_LEFT, 0, -10);
-    lv_obj_align_to(zmk_widget_layer_status_obj(&layer_status_widget), zmk_widget_modifiers_obj(&modifiers_widget), LV_ALIGN_OUT_TOP_LEFT, 0, -10);
+    lv_obj_align_to(zmk_widget_layer_status_obj(&layer_status_widget), zmk_widget_modifiers_obj(&modifiers_widget), LV_ALIGN_OUT_TOP_LEFT, 0, -15);
 
     zmk_widget_dongle_battery_status_init(&dongle_battery_status_widget, screen);
     lv_obj_align(zmk_widget_dongle_battery_status_obj(&dongle_battery_status_widget), LV_ALIGN_TOP_RIGHT, 0, 0);
