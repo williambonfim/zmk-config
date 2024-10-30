4a9125705481a4ce43b73ad0becb02be705d3619
williambonfim <gbo.william@gmail.com>
2024-10-30 13:00:54 +0000

    LVGL_BACKGROUND

--- config/boards/shields/hammerbeam_display/widgets/peripheral_status.c
+++ config/boards/shields/hammerbeam_display/widgets/peripheral_status.c
@@ -24,6 +24,7 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #include "peripheral_status.h"
 
 #define LVGL_FOREGROUND lv_color_hex(0xFFFFFF)  // Replace with desired color
+#define LVGL_BACKGROUND lv_color_hex(0x000000)  // Set to black or another color
 
 LV_IMG_DECLARE(hammerbeam1);
 LV_IMG_DECLARE(hammerbeam2);
